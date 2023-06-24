f87de73d8c5accd67b2e0044cc14425fb5cf2f06
Kamil Khabibullin <kamil_khabibullin@bk.ru>
2023-06-24 17:41:28 +0300

    prod version

--- app/main.py
+++ app/main.py
@@ -10,7 +10,6 @@ from .utils.scripts.converter import converter
 from config import load_config
 from .models.images import Polygons
 
-
 app = FastAPI()
 
 IMAGEDIR = "app/images/"
@@ -42,48 +41,61 @@ async def root():
 async def create_upload_file(file: UploadFile = File(...)):
     session = await create_db_session(cfg=config)
     name = uuid.uuid4()
-    file.filename = f"{name}.tif"
-    contents = await file.read()
+    try:
+        file.filename = f"{name}.tif"
+        contents = await file.read()
 
-    with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
-        f.write(contents)
+        with open(f"{IMAGEDIR}{file.filename}", "wb") as f:
+            f.write(contents)
 
-    result, geojson_path, full_coordinates = converter(name)
+        result, geojson_path, full_coordinates = converter(name)
 
-    now = datetime.now()
-    await Polygons.insert_polygons(session_maker=session, name=str(name), images="path_to_dir",
-                                   full_coordinates=full_coordinates, date_publish=now)
+        now = datetime.now()
+        await Polygons.insert_polygons(session_maker=session, name=str(name), images="path_to_dir",
+                                       full_coordinates=full_coordinates, date_publish=now)
 
-    return result
+        return result
+    except Exception as e:
+        print(e)
+        return "Кажется данное изображение не имеет геоданных."
 
 
 @app.get('/polygons')
 async def get_markers_coords():
-    # получение из бд
-    new_mass = []
     session = await create_db_session(cfg=config)
-    # for item in mass:
-    #     new_item = {
-    #         type: "Feature",
-    #         'geometry': {
-    #             type: "Polygon",
-    #             'coordinates': item.full_coordinates
-    #
-    #         },
-    #         id: item.id,
-    #         'properties': {
-    #             'images': item.images,
-    #             'datePublish': item.dataPublish
-    #         }
-    #     }
-    #     new_mass.append(new_item)
-    #
-    # newPolygonJSON = {
-    #     type: "FeatureCollection",
-    #     'features': new_mass
-    # }
-    return await Polygons.get_polygons(session_maker=session)
+    new_mass = []
+    try:
+        mass = [await Polygons.get_polygons(session_maker=session)]
+    except Exception:
+        return "Возникли проблемы на сервер, попробуйте позже."
+    if len(mass) == 0:
+        return "Изображений нет"
+    else:
+        for lst in mass:
+            for item in lst:
+                polygon = item.Polygons
+                new_item = {
+                    'type': "Feature",
+                    'geometry': {
+                        'type': "Polygon",
+                        'coordinates': polygon.full_coordinates
+
+                    },
+                    'id': polygon.id,
+                    'properties': {
+                        'images': polygon.images,
+                        'datePublish': polygon.datePublish
+                    }
+                }
+                new_mass.append(new_item)
+
+        newPolygonJSON = {
+            'type': "FeatureCollection",
+            'features': new_mass
+        }
+        return newPolygonJSON
+
 
 if __name__ == "__main__":
     async def start():
-        uvicorn.run(app, host="0.0.0.0", port=8000)
+        uvicorn.run(app, host="127.0.0.1", port=8000)
