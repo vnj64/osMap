47ce8600366b0977445a4af576db9ae8bacefe49
Kamil Khabibullin <kamil_khabibullin@bk.ru>
2023-06-25 18:09:35 +0300

    PEP corrections

--- app/test_main.py
+++ app/test_main.py
@@ -2,8 +2,8 @@ import pytest
 from fastapi.testclient import TestClient
 from .main import app
 
-
 client = TestClient(app)
+
 
 def test_read_root():
     response = client.get("/")
@@ -11,11 +11,9 @@ def test_read_root():
     assert response.json() == {"msg": "Hello World"}
 
 
-    
 def test_read_create_upload_file():
     response = client.get("/api/upload/")
     assert response.status_code == 200
-
 
 
 def test_read_get_markers_coords():
